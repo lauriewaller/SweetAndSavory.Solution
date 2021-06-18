d360fb0de71554ac99781a0b4ee663646e4c2669
Laurie Waller <lauriewaller7@gmail.com>
2021-06-17 19:28:37 -0700

    create models

---
+++ SweetAndSavory/Models/Treat.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+
+namespace SweetAndSavory.Models
+{
+  public class Treat
+  {
+    public Treat()
+    {
+      this.JoinEntities = new HashSet<FlavorTreat>();
+    }
+
+    public int TreatId { get; set; }
+    public string Name { get; set; }
+
+    public virtual ICollection<FlavorTreat> JoinEntities { get; }
+  }
+}
